31a892cc98ea792eaa0177a45944105aa8e127b6
DMTaira <mitiko.kita@gmail.com>
2023-12-13 22:38:23 -0300

    Inclusão do método pesquisarFilme em OperacoesFilme e inclusão de teste em IMDB

--- adatech/poo/imdb/IMDB.java
+++ adatech/poo/imdb/IMDB.java
@@ -48,11 +48,7 @@ public class IMDB {
             " pela vida da dona de casa Francesca Johnson (Meryl Streep) por quatro dias na década de 1960.",
             "Drama romântico",LocalDate.of(1995,05,16),15000000.00,
             "Clint Eastwood", new String[]{"Clint Eastwood", "Merryl Streep"});
-//        filme = ("As pontes de Madison";"O fotógrafo Robert Kincaid (Clint Eastwood) vagou" +
-//                " pela vida da dona de casa Francesca Johnson (Meryl Streep) por quatro dias na década de 1960.",
-//                "Drama romântico", "Clint Eastwood", "1995", String.valueOf(15000000.00),
-//                "Clint Eastwood", "Merryl Streep");
-//
+
         Filme filme1 = new Filme("Interestelar","Uma equipe de exploradores viaja através de um buraco" +
           "de minhoca no espaço, na tentativa de garantir a sobrevivência da humanidade.","Aventura, Ficção Científica",
           LocalDate.of(2014,07,15),1000000.00,"Christopher Nolan",new String []
@@ -61,6 +57,9 @@ public class IMDB {
         OperacoesFilme.incluirFilme (filme);
         OperacoesFilme.incluirFilme (filme1);
         System.out.println("Filme incluido com sucesso.");
+
+        Filme filmeApesquisar = OperacoesFilme.pesquisarFilme("As pontes de Madison");
+        filmeApesquisar = OperacoesFilme.pesquisarFilme("Seven");
 
     }
 
@@ -84,7 +83,7 @@ public class IMDB {
         elenco[0].display();
         elenco[1] = new Ator("Deyse","27/02/1968","Rio de Janeiro",filmes);
         elenco[1].display();
-    }
+   }
 
     /* Método para testar a funcionalidade da classe Diretor.
        Todas as alterações devem ser incluídas aqui nesta
