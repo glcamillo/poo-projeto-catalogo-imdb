56686818df839f7467413e9e9a9692a9cb96df67
Deyse Ribeiro <deyse.ribeiro.imoveis@gmail.com>
2023-12-15 05:21:50 -0300

    Modificando a classe Ator e adicionando a classe Elenco.

--- adatech/poo/imdb/Ator.java
+++ adatech/poo/imdb/Ator.java
@@ -1,80 +1,60 @@
+package adatech.poo.imdb;
 import java.util.Arrays;
-//Ator.class
+import java.util.ArrayList;
+
+// Classe que representa um ator
 public class Ator {
-    //private final int NUM_ATORES = 100;
     private String nome;
     private String dataNascimento;
     private String localNascimento;
-    private String[] filmes;
-    private String[] ficha = new String[4];
+    private ArrayList<String> filmes;
 
-
-    public Ator(String nome, String dataNascimento, String localNascimento, String[] filmes) {
+    // Construtor para inicializar um objeto Ator
+    public Ator(String nome, String dataNascimento, String localNascimento, ArrayList<String> filmes) {
         this.nome = nome;
         this.dataNascimento = dataNascimento;
         this.localNascimento = localNascimento;
         this.filmes = filmes;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(String dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public String getLocalNascimento() {
-        return localNascimento;
-    }
-
-    public void setLocalNascimento(String localNascimento) {
-        this.localNascimento = localNascimento;
-    }
-
-    public String[] getFilmes() {
-        return filmes;
-    }
-
-    public void setFilmes(String filmes[]) {
-        this.filmes = filmes;
-    }
-
-    public String[] getFicha(){
-        this.ficha[0] = this.nome;
-        this.ficha[1] = this.dataNascimento;
-        this.ficha[2] = this.localNascimento;
-        this.ficha[3] = Arrays.toString(this.filmes);
-        return ficha;
-    }
+    // Método para exibir informações sobre o ator
     public void display() {
         System.out.println("O ator: " + nome + " nasceu em " + dataNascimento + " em " + localNascimento +
-                " e fez os filmes: " + Arrays.toString(filmes));
-        System.out.println("Ficha:" + Arrays.toString(getFicha()));
+                " e fez os filmes: " + filmes);
+    }
+
+    // Método para obter o nome do ator
+    public String getNome(){
+        return nome;
     }
 }
-//public class Elenco{
-//Ator[] elenco;
-//elenco = new Ator[5];
-//}
-/*
-class Main{
-    public static void main(String[] args) {
-        Ator[] elenco;
-        elenco = new Ator[5];
-        String[] filmes = {"volvo", "BMW", "Ford", "Mazda"};
-        elenco[0] = new Ator("Gustavo","10/11/1999","Nova Iguaçu",filmes);
-        elenco[0].display();
-        elenco[1] = new Ator("Deyse","27/02/1968","Rio de Janeiro",filmes);
-        elenco[1].display();
 
+// Classe que representa um elenco de atores
+class Elenco {
+    // Lista de atores no elenco
+    ArrayList<Ator> elenco = new ArrayList<>();
+
+    // Construtor padrão
+    public Elenco() {
     }
-}*/
+
+    // Método para adicionar um ator ao elenco
+    public void setElenco(String nome, String dataNascimento, String localNascimento, ArrayList<String> filmes) {
+        Ator ator = new Ator(nome, dataNascimento, localNascimento, filmes);
+        elenco.add(ator);
+    }
+
+    // Método para obter a lista completa de atores no elenco
+    public ArrayList<Ator> getElenco() {
+        return elenco;
+    }
+
+    // Método para obter os nomes dos atores no elenco
+    public ArrayList<String> getAtores() {
+        ArrayList<String> atores = new ArrayList<>();
+        for (Ator ator : elenco) {
+            atores.add(ator.getNome());
+        }
+        return atores;
+    }
+}
