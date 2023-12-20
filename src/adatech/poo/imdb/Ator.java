5549c5f3487c70298ea95a96a99e6ef372f12e25
Deyse Ribeiro <deyse.ribeiro.imoveis@gmail.com>
2023-12-20 02:03:17 -0300

    Modificando o código da classe Ator para ficar semelhante ao de Diretor e funcionar como extensão da classe pessoa.

--- src/adatech/poo/imdb/Ator.java
+++ src/adatech/poo/imdb/Ator.java
@@ -1,38 +1,40 @@
 package src.adatech.poo.imdb;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.time.LocalDate;
 
-public class Ator {
-    private String nome;
-    private String dataNascimento;
-    private String localNascimento;
-    private ArrayList<String> filmes;
+public class Ator extends Pessoa {
+    private boolean atorEmFilme;
 
-    private ArrayList<String> atores = new ArrayList<>();
-    private ArrayList<String> listaNomes = new ArrayList<>();
-
-    public Ator(){
-
+    public Ator(String nomeAtor, LocalDate dataNascimento, String nacionalidade, boolean atorEmFilme) {
+        super(nomeAtor, dataNascimento, nacionalidade);
+        this.atorEmFilme = atorEmFilme;
     }
-    public void cadastrarAtor(String nome, String dataNascimento, String localNascimento, ArrayList<String> filmes) {
-        this.nome = nome;
-        this.dataNascimento = dataNascimento;
-        this.localNascimento = localNascimento;
-        this.filmes = filmes;
-        ArrayList<String> ficha = new ArrayList<>();
-        ficha.add("Nome: " + this.nome);
-        ficha.add("Data de Nascimento: " + this.dataNascimento);
-        ficha.add("Local de Nascimento: " + this.localNascimento);
-        ficha.add("Filmes: " + Arrays.toString(this.filmes.toArray()));
 
-        atores.add(Arrays.toString(ficha.toArray()));
-        listaNomes.add(this.nome);
+    public Ator(String nome, boolean atorEmFilme) {
+        super(nome);
+        this.atorEmFilme = atorEmFilme;
     }
-    public ArrayList<String> getAtores() {
-        return atores;
+
+    public Ator(String nome, LocalDate dataNascimento, boolean atorEmFilme) {
+        super(nome, dataNascimento);
+        this.atorEmFilme = atorEmFilme;
     }
-    public ArrayList<String> getNomeAtores() {
-        return listaNomes;
+
+    public String getNomeAtor() {
+        return super.getNome();
+    }
+
+    public void setAtorEmFilme(boolean atorEmFilme) {
+        this.atorEmFilme = atorEmFilme;
+    }
+
+    @Override
+    public String toString() {
+        return "Ator{" +
+                "atorEmFilme=" + atorEmFilme +
+                ", nome='" + nome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", nacionalidade='" + nacionalidade + '\'' +
+                '}';
     }
 }
